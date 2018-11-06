097ec1c70610589f02f3bffc0533273ba944fa01
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2018-11-06 21:10:18 -0200

    melhorias na usabilidade + pagina de contato

--- public/js/cifraCesar.js
+++ public/js/cifraCesar.js
@@ -1,6 +1,6 @@
 function cifraCesar(string, chave) {
 
-  const alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','t','s','t','u','v','w','x','y','z'];
+  const alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','t','s','t','u','v','w','x','y','z',' '];
   let result = '';
 
   for(let i = 0; i < string.length; i++){
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
+var processos = document.getElementById('processos');
+var table = document.getElementById('table-result');
+var numero_processo = document.getElementById('numero_processo');
+var total_processos = document.getElementById('total_processos');
+
 document.getElementById("btnDecodificar").addEventListener("click", function(){
   let input = document.getElementById('input').value;
   let tentativas = parseInt(document.getElementById('tentativas').value);
+
+  table.innerHTML = "";
 
   main(input, tentativas);
 });
@@ -12,14 +19,10 @@ function sleep(ms) {
 
 async function main(input, tentativas) {
 
-  let processos = document.getElementById('processos');
   processos.classList.remove('d-none');
-  let numero_processo = document.getElementById('numero_processo');
   numero_processo.innerHTML = 1;
-  let total_processos = document.getElementById('total_processos');
   total_processos.innerHTML = 1 + tentativas;
 
-  let table = document.getElementById('table-result');
   table.classList.remove('d-none');
   let row = table.insertRow(0);
   let cell1 = row.insertCell(0);
@@ -37,9 +40,10 @@ async function main(input, tentativas) {
     cell1 = row.insertCell(0);
     cell2 = row.insertCell(1);
 
-    cell1.innerHTML = "Cifra de Cesar ("+(i+1)+"):";
+    cell1.innerHTML = "Cifra de César ("+(i+1)+"):";
     cell2.innerHTML = cifraCesar(input,i);
 
     await sleep(2000);
   }
+  processos.classList.add('d-none');
 }
